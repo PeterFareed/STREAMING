287e93f4717ac9b934b0acc99a470111af842e23
PeterFareed <46231250+PeterFareed@users.noreply.github.com>
2019-01-01 15:11:41 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+```js
+client.on('ready', (&st) => {
+   console.log(`----------------`);
+      console.log(`By Venom#4403`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Venom#4403 ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`اينما وجد الحب وجد السلام ❤`,"http://twitch.tv/ahmad107996")
+client.user.setStatus("dnd")
+})
